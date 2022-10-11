75e6a6f1518b03313ce31f6181971fe0433e9362
Serhii Kuhaiev <kugaew@gmail.com>
2022-10-11 19:00:20 +0200

    test save ex-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
+/* const inputEmailEl = document.querySelector('input[name="email"]');
+const inputMassageEl = document.querySelector('textarea[name="message"]'); */
+const formEl = document.querySelector('form.feedback-form');
+const obj = {};
+const INPUT_FORM_KEY = 'feedback-form-state';
 
+const dataFromLocalStorage = JSON.parse(localStorage.getItem(INPUT_FORM_KEY));
+
+if (dataFromLocalStorage) {
+  if (dataFromLocalStorage.email) {
+    formEl.querySelector('input[name="email"]').value =
+      dataFromLocalStorage.email;
+  }
+
+  if (dataFromLocalStorage.message) {
+    formEl.querySelector('textarea[name="message"]').value =
+      dataFromLocalStorage.message;
+  }
+}
+
+formEl.addEventListener('input', throttle(onWriteDataToLocalStorage, 500));
+
+function onWriteDataToLocalStorage(evt) {
+  /* localStorage.setItem(
+    INPUT_FORM_KEY,
+    JSON.stringify({ [evt.target.getAttribute('name')]: evt.target.value })
+  ); */
+
+  obj[evt.target.getAttribute('name')] = evt.target.value;
+  localStorage.setItem(INPUT_FORM_KEY, JSON.stringify(obj));
+}
