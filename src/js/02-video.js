96f29c77222c22730848249dd0ebdd50ff659c7c
Serhii Kuhaiev <kugaew@gmail.com>
2022-10-11 21:55:53 +0200

    add JSON format in ex-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,19 +3,20 @@ import throttle from 'lodash.throttle';
 
 const iframeEl = document.querySelector('#vimeo-player');
 const player = new Player(iframeEl);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
-const CURRENT_TIME_KEY = localStorage.getItem('videoplayer-current-time');
+const isNotEmpryStorage = localStorage.getItem(CURRENT_TIME_KEY);
 
-if (CURRENT_TIME_KEY) {
+if (isNotEmpryStorage) {
   onSetCurrentVideoTime();
 }
 
 player.on('timeupdate', throttle(onGetCurrentVideoTime, 1000));
 
 function onGetCurrentVideoTime(event) {
-  localStorage.setItem('videoplayer-current-time', event.seconds);
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(event.seconds));
 }
 
 function onSetCurrentVideoTime() {
-  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+  player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)));
 }
