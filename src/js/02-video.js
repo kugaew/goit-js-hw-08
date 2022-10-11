90d63afa196eea16b87bf21669a99201d51d8677
Serhii Kuhaiev <kugaew@gmail.com>
2022-10-11 15:37:17 +0200

    make exercise-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
+
+const CURRENT_TIME_KEY = localStorage.getItem('videoplayer-current-time');
+
+if (CURRENT_TIME_KEY) {
+  onSetCurrentVideoTime();
+}
+
+player.on('timeupdate', throttle(onGetCurrentVideoTime, 1000));
+
+function onGetCurrentVideoTime(event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}
+
+function onSetCurrentVideoTime() {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
